aac43367fbc496abe257fd6fb5093a0a5b37b71e
Pylar <164821928+deafdudecomputers@users.noreply.github.com>
2025-12-18 22:13:56 -0800

    v1.1.35

--- setup_freeze.py
+++ setup_freeze.py
@@ -63,7 +63,7 @@ base=None
 if sys.platform=="win32": base="Console"
 setup(
     name="PalworldSaveTools",
-    version="1.1.34",
+    version="1.1.35",
     description="All-in-one tool for fixing/trans ferring/editing Palworld saves",
     options={"build_exe":build_exe_options},
     executables=[
